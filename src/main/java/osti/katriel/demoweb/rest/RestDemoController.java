e9a5f4b1f0a9b1fd2a712550c534025f213c2ba4
Osti Katriel <katriel181920@example.com>
2021-03-02 12:14:59 -0500

    REST basico completo y instalacion de spring security

--- src/main/java/osti/katriel/demoweb/rest/RestDemoController.java
+++ src/main/java/osti/katriel/demoweb/rest/RestDemoController.java
@@ -3,7 +3,12 @@ package osti.katriel.demoweb.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +27,27 @@ public class RestDemoController {
 
 		return personaRepo.findAll();
 
+	}
+	
+	@PostMapping
+	public void insertar(@RequestBody Persona persona) {
+		
+		personaRepo.save(persona);
+		
+	}
+	
+	@PutMapping
+	public void modificar(@RequestBody Persona persona) {
+		
+		personaRepo.save(persona);
+		
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public void eliminar(@PathVariable("id") Integer id) {
+		
+		personaRepo.deleteById(id);
+		
 	}
 	
 }
